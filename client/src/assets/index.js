d849769c308c6f4e4cc88a3c3a3bea6c72924c4f
Ali haider <i.alihaidermufti10@gmail.com>
2023-06-27 19:30:19 +0500

    First commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import deleteIcon from './delete.png';
+
+export {
+  download,
+  logo,
+  preview,
+  deleteIcon,
+};
